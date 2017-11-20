4380c4bbb59d6784707a5c6aba1a36f9b1b77a3f
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-11-20 12:20:13 +0530

    Acceptance: Subsription test cases fixed

--- tests/acceptance/subscriptions-test.js
+++ tests/acceptance/subscriptions-test.js
@@ -1,52 +1,50 @@
-// import Ember from 'ember';
-// import startApp from '../helpers/start-app';
-// import syncDataStub from '../helpers/empty-sync-data-stub';
-// import FactoryGuy from 'ember-data-factory-guy';
-// import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
-// import testSkip from '../helpers/test-skip';
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+import FactoryGuy from 'ember-data-factory-guy';
+import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
+import testSkip from '../helpers/test-skip';
 
-// var App;
+var App;
 
-// module('Subscriptions', {
-//   beforeEach: function() {
-//     App = startApp();
-//     TestHelper.setup();
-//     syncDataStub(TestHelper);
-//   },
-//   afterEach: function() {
-//     Em.run(function() { TestHelper.teardown(); });
-//     Ember.run(App, 'destroy');
-//   }
-// });
+module('Subscriptions', {
+  beforeEach: function() {
+    App = startApp();
+    TestHelper.setup();
+  },
+  afterEach: function() {
+    Em.run(function() { TestHelper.teardown(); });
+    Ember.run(App, 'destroy');
+  }
+});
 
-// testSkip("updateStore doesn't process before response to model.save request", function() {
-//   expect(2);
+testSkip("updateStore doesn't process before response to model.save request", function() {
+  expect(2);
 
-//   var store = FactoryGuy.store;
-//   var subscriptions = lookup('controller:subscriptions');
-//   var user = FactoryGuy.make('user');
-//   FactoryGuy.make('user_profile', {id:user.id});
-//   var offer = {id:2};
+  var store = FactoryGuy.store;
+  var subscriptions = lookup('controller:subscriptions');
+  var user = FactoryGuy.make('user');
+  FactoryGuy.make('user_profile', {id:user.id});
+  var offer = {id:2};
 
-//   $.mockjaxSettings.logging = true;
-//   $.mockjax({url:"/api/v1/offer*",status:201,response: function(){
-//     Ember.run(function() {
-//       subscriptions.update_store({
-//         item: {"offer":offer},
-//         sender: {"user":user.toJSON({includeId:true})},
-//         operation: "create"
-//       }, function(){});
-//     });
-//     this.responseText = {"offer":offer};
-//   }});
+  $.mockjaxSettings.logging = true;
+  $.mockjax({url:"/api/v1/offer*",status:201,response: function(){
+    Ember.run(function() {
+      subscriptions.update_store({
+        item: {"offer":offer},
+        sender: {"user":user.toJSON({includeId:true})},
+        operation: "create"
+      }, function(){});
+    });
+    this.responseText = {"offer":offer};
+  }});
 
-//   Ember.run(function() {
-//     store.createRecord("offer",{createdBy:user}).save().then(function() {
-//       equal(store.all("offer").get("length"), 1);
-//       equal(store.all("offer").get("firstObject.id"), offer.id);
-//     });
-//   });
+  Ember.run(function() {
+    store.createRecord("offer",{createdBy:user}).save().then(function() {
+      equal(store.all("offer").get("length"), 1);
+      equal(store.all("offer").get("firstObject.id"), offer.id);
+    });
+  });
 
-//   // causes test to wait for next ember run loop before completing
-//   andThen(function() { });
-// });
+  // causes test to wait for next ember run loop before completing
+  andThen(function() { });
+});
