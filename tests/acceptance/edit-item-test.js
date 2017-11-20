50a249ebc514bfb695343e5c9161aad6c7ac6b3b
Darshan Chauhan <darshan.chauhan@kiprosh.com>
2017-11-20 17:49:35 +0530

    Acceptance: 2 edit item test cases fixed

--- tests/acceptance/edit-item-test.js
+++ tests/acceptance/edit-item-test.js
@@ -1,31 +1,31 @@
-// import Ember from 'ember';
-// import startApp from '../helpers/start-app';
-// import syncDataStub from '../helpers/empty-sync-data-stub';
-// import FactoryGuy from 'ember-data-factory-guy';
-// import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+//import syncDataStub from '../helpers/empty-sync-data-stub';
+import FactoryGuy from 'ember-data-factory-guy';
+import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
 
-// var App, offer, offer1, item, item1;
+var App, offer, offer1, item, item1;
 
-// module('Edit Item', {
-//   beforeEach: function() {
-//     App = startApp();
-//     TestHelper.setup(App);
-//     syncDataStub(TestHelper);
+module('Edit Item', {
+  beforeEach: function() {
+    App = startApp();
+    TestHelper.setup(App);
+    //syncDataStub(TestHelper);
 
-//     offer = FactoryGuy.make("offer", { state: "draft" });
-//     item = FactoryGuy.make("item",{ offer:offer, state: "draft" });
+    offer = FactoryGuy.make("offer", { state: "draft" });
+    item = FactoryGuy.make("item",{ offer:offer, state: "draft" });
 
-//     offer1 = FactoryGuy.make("offer", { state: "draft" });
-//     item1 = FactoryGuy.make("item",{ offer:offer1, state: "submitted",
-//       donorDescription: "this is a test Item"});
-//     FactoryGuy.makeList("donor_condition", 2);
-//   },
+    offer1 = FactoryGuy.make("offer", { state: "draft" });
+    item1 = FactoryGuy.make("item",{ offer:offer1, state: "submitted",
+      donorDescription: "this is a test Item"});
+    FactoryGuy.makeList("donor_condition", 2);
+  },
 
-//   afterEach: function() {
-//     Em.run(function() { TestHelper.teardown(); });
-//     Ember.run(App, 'destroy');
-//   }
-// });
+  afterEach: function() {
+    Em.run(function() { TestHelper.teardown(); });
+    Ember.run(App, 'destroy');
+  }
+});
 
 // test("Create Item with details", function() {
 //   expect(4);
@@ -54,39 +54,39 @@
 //   });
 // });
 
-// test("Discard Item with details", function() {
-//   var edit_item_url = "/offers/" + offer.id + "/items/" + item.id + "/edit";
-//   visit(edit_item_url);
+test("Discard Item with details", function() {
+  var edit_item_url = "/offers/" + offer.id + "/items/" + item.id + "/edit";
+  visit(edit_item_url);
 
-//   andThen(function() {
-//     equal(currentURL(), edit_item_url);
-//   });
+  andThen(function() {
+    equal(currentURL(), edit_item_url);
+  });
 
-//   fillIn("textarea[name=donorDescription]", "this will be discarded");
+  fillIn("textarea[name=donorDescription]", "this will be discarded");
 
-//   TestHelper.handleDelete("item", item.id);
-//   click(".button:contains('Cancel')");
+  TestHelper.handleDelete("item", item.id);
+  click(".button:contains('Cancel')");
 
-//   andThen(function(){
-//     equal(currentURL(), "/offers/"+ offer.id);
-//     equal($('.item-content li').length, 0);
-//   });
-// });
+  andThen(function(){
+    equal(currentURL(), "/offers/"+ offer.id);
+    equal($('.item-content li').length, 0);
+  });
+});
 
-// test("Discard changes for existing Item", function() {
-//   var edit_item_url = "/offers/" + offer1.id + "/items/" + item1.id + "/edit";
-//   visit(edit_item_url);
+test("Discard changes for existing Item", function() {
+  var edit_item_url = "/offers/" + offer1.id + "/items/" + item1.id + "/edit";
+  visit(edit_item_url);
 
-//   andThen(function() {
-//     equal(currentURL(), edit_item_url);
-//   });
+  andThen(function() {
+    equal(currentURL(), edit_item_url);
+  });
 
-//   fillIn("textarea[name=donorDescription]", "changes for existing item");
+  fillIn("textarea[name=donorDescription]", "changes for existing item");
 
-//   click(".button:contains('Cancel')");
+  click(".button:contains('Cancel')");
 
-//   andThen(function(){
-//     equal(currentURL(), "/offers/"+ offer1.id +"/offer_details");
-//     equal($('.item-content li:eq(0) .ellipsis').text(), "this is a test Item");
-//   });
-// });
+  andThen(function(){
+    equal(currentURL(), "/offers/"+ offer1.id +"/offer_details");
+    equal($('.item-content li:eq(0) .ellipsis').text(), "this is a test Item");
+  });
+});
