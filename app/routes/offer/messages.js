2672ca24952e800c91bbbe269abed59a50f7a194
namrataukirde <namrata.ukirde@kiprosh.com>
2018-05-24 16:44:52 +0530

    fix test cases

--- app/routes/offer/messages.js
+++ app/routes/offer/messages.js
@@ -2,7 +2,7 @@ import MessagesBaseRoute from 'shared-goodcity/routes/messages_base';
 
 export default MessagesBaseRoute({
   afterModel(model) {
-    var offerId = offer.id;
+    var offerId = model.id;
     this.store.query('version', { offer_id: offerId, for_offer: true });
   }
 });
