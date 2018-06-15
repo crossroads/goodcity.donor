5dd2e23dd8d1f80e97cedb2db8f879066714a2a5
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-06-15 08:52:27 +0530

    AppReview service call removed

--- app/routes/offer/index.js
+++ app/routes/offer/index.js
@@ -3,11 +3,8 @@ import Ember from 'ember';
 
 export default AuthorizeRoute.extend({
   controllerName: 'offer/offer_details',
-  appReview: Ember.inject.service(),
 
   afterModel(my_offer) {
-    this.get("appReview").promptReviewModal();
-
     if(!my_offer) {
       this.transitionTo('offers');
     } else if(my_offer && my_offer.get('itemCount') > 0) {
