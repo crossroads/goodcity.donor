fb3729a1c7dc11487debc4cf93774345117e9cfb
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-12-11 09:41:55 +0530

    Jcenter dependency removed and maven dependency added

---
+++ cordova/scripts/maven_swap.sh
@@ -0,0 +1,16 @@
+#Remove when cordova-android is upgraded to 7.1.2
+#!/bin/bash
+if [ "$CI_SERVER" = "yes" ]; then
+  # If CI_SERVER is undefined or 'no' then the build is not running in Ionic Pro
+  echo 'Ionic Pro build detected, proritizing maven'
+  #remove jcenter
+  sed -i '/jcenter()/d' ./platforms/android/CordovaLib/build.gradle
+  #append jcenter
+  sed -i '/maven {/{
+  N
+  N
+  a\
+  jcenter()
+  }' ./platforms/android/CordovaLib/build.gradle
+  cat ./platforms/android/CordovaLib/build.gradle
+fi
