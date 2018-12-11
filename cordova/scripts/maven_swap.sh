ebcacbc21b27ca6abe9111617f297c9b310035a0
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-12-11 09:47:57 +0530

    Typo fixed

--- cordova/scripts/maven_swap.sh
+++ cordova/scripts/maven_swap.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 if [ "$CI_SERVER" = "yes" ]; then
   # If CI_SERVER is undefined or 'no' then the build is not running in Ionic Pro
-  echo 'Ionic Pro build detected, proritizing maven'
+  echo 'Cordova android build detected, proritizing maven'
   #remove jcenter
   sed -i '/jcenter()/d' ./platforms/android/CordovaLib/build.gradle
   #append jcenter
