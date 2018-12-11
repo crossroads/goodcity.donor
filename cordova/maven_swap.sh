4fe0e5ddf1d110d198f91324dcb3cb5418786ce6
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-12-11 11:53:02 +0530

    Script added to add google repo

--- cordova/maven_swap.sh
+++ cordova/maven_swap.sh
@@ -1,6 +1,6 @@
 #Remove when cordova-android is upgraded to 7.1.2
 #!/bin/bash
-echo 'Cordova android build detected, proritizing maven'
+echo 'Cordova android build detected, prioritizing maven'
 #remove jcenter
 sed -i '/jcenter()/d' ./platforms/android/CordovaLib/build.gradle
 #append jcenter
