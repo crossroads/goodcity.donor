e97e42056c775fd52b6098164cdb64ed3b914e1a
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-12-26 11:13:02 +0530

    Revert "GCW-2283: Correct version for staging"
    This reverts commit f28618e3d7e335b63632f86c7c012dfeee0ed798.

--- config/environment.js
+++ config/environment.js
@@ -63,6 +63,7 @@ module.exports = function(environment) {
       PRELOAD_AUTHORIZED_TYPES: ["donor_condition","permission", "timeslot", "gogovan_transport", "crossroads_transport", "version"],
       SHA: process.env.APP_SHA || "00000000",
       SHARED_SHA:  process.env.APP_SHARED_SHA || "00000000",
+      VERSION: pkgJson.version || "1.0.0",
       ANDROID_APP_ID: "hk.goodcity.app",
       IOS_APP_ID: "1012253845"
     },
@@ -90,7 +91,6 @@ module.exports = function(environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
 
     // RESTAdapter Settings
-    ENV.VERSION: "1.0.0";
     ENV.APP.API_HOST_URL = 'http://localhost:3000';
     ENV.APP.SOCKETIO_WEBSERVICE_URL = 'http://localhost:1337/goodcity';
 
@@ -112,7 +112,6 @@ module.exports = function(environment) {
   }
 
   if (environment === 'test') {
-    ENV.VERSION: "1.0.0";
     ENV.cordova.enabled = false;
     // Testem prefers this...
     ENV.baseURL = '/';
@@ -130,7 +129,6 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // RESTAdapter Settings
-    ENV.VERSION: pkgJson.version;
     ENV.APP.API_HOST_URL = 'https://api.goodcity.hk';
     ENV.APP.SOCKETIO_WEBSERVICE_URL = 'https://socket.goodcity.hk:81/goodcity';
 
@@ -150,7 +148,6 @@ module.exports = function(environment) {
 
   if ((process.env.staging || process.env.STAGING) === 'true') {
     ENV.staging = true;
-    ENV.VERSION = process.env.VERSION;
     ENV.APP.API_HOST_URL = 'https://api-staging.goodcity.hk';
     ENV.APP.SOCKETIO_WEBSERVICE_URL = 'https://socket-staging.goodcity.hk:81/goodcity';
     ENV.contentSecurityPolicy["connect-src"] = [
