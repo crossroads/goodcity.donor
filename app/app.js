f5b0932078707119b93e752afda02faa868c5fcb
namrataukirde <namrata.ukirde@kiprosh.com>
2019-07-29 20:03:28 +0530

    fix MODEL_FACTORY_INJECTIONS deprecation

--- app/app.js
+++ app/app.js
@@ -1,14 +1,12 @@
-import Ember from 'ember';
-import Resolver from './resolver';
-import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import Ember from "ember";
+import Resolver from "./resolver";
+import loadInitializers from "ember-load-initializers";
+import config from "./config/environment";
 
 let App;
 
 window.EmberENV = window.EmberENV || {};
 window.EmberENV.ENABLE_DS_FILTER = true;
-
-Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
