570c1bb4051df90efaa86bcf0cb26b4f80871210
saunmanoj888 <manoj@kiprosh.com>
2021-04-21 00:44:14 +0530

    GCW-3587 - Set back link on authenticate page to redirect to register or login page

--- app/routes/authenticate.js
+++ app/routes/authenticate.js
@@ -1,15 +1,31 @@
-import Ember from 'ember';
+import Ember from "ember";
 const { getOwner } = Ember;
 
 export default Ember.Route.extend({
+  backLinkPath: Ember.computed.localStorage(),
 
   beforeModel() {
-    if (this.session.get('isLoggedIn')) {
-      if(this.get('session.currentUser')) {
-        this.transitionTo('/offers');
+    if (this.session.get("isLoggedIn")) {
+      if (this.get("session.currentUser")) {
+        this.transitionTo("/offers");
       } else {
-        getOwner(this).lookup("route:application")._loadDataStore();
+        getOwner(this)
+          .lookup("route:application")
+          ._loadDataStore();
       }
     }
+
+    let previousRoutes = this.router.router.currentHandlerInfos;
+    let previousRoute = previousRoutes && previousRoutes.pop().name;
+    if (previousRoute) {
+      this.set("backLinkPath", previousRoute);
+    } else {
+      this.set("backLinkPath", "login");
+    }
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set("backLinkPath", this.get("backLinkPath"));
   }
 });
