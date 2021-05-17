e6a766f40ff7c8471407bdce67bb050a733921fa
shreyas098 <shreyash@kiprosh.com>
2021-05-17 14:37:46 +0530

    Remove test suite for adding mirage setup

--- tests/factories/mobile.js
+++
@@ -1,9 +0,0 @@
-export default {
-  hongKong: function() {
-    return Math.floor(Math.random() * 8999922 + 67110000).toString();
-  },
-
-  nonHongKong: function() {
-    return Math.floor(Math.random() * 8999922 + 671100001).toString();
-  }
-};
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,16 @@
-import resolver from './helpers/resolver';
-import {
-  setResolver
-} from 'ember-qunit';
+// // import resolver from './helpers/resolver';
+// // import {
+// //   setResolver
+// // } from 'ember-qunit';
 
-setResolver(resolver);
+// // setResolver(resolver);
+// import Application from "../app";
+// import config from "../config/environment";
+// import { setApplication } from "@ember/test-helpers";
+// // import { startMirage } from 'goodcity/initializers/ember-cli-mirage';
+
+// import { start } from "ember-qunit";
+
+// setApplication(Application.create(config.APP));
+// // startMirage(Application.create(config.APP))
+// start();
