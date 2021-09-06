ed9168d24f78a00df1896ec9ad7e7f288a6f3329
shreyas098 <shreyash@kiprosh.com>
2021-09-06 17:14:56 +0530

    Export external link component to app.goodcity

---
+++ app/components/external-link.js
@@ -0,0 +1,25 @@
+import Ember from "ember";
+import config from "../config/environment";
+
+export default Ember.Component.extend({
+  tagName: "a",
+  href: "#",
+  cordova: Ember.inject.service(),
+
+  click() {
+    if (config.cordova.enabled) {
+      cordova.InAppBrowser.open(
+        "https://www.crossroads.org.hk/home/donate-funds/",
+        "_system"
+      );
+    } else {
+      window.open(this.decodeLink(), "_system");
+    }
+    return false;
+  },
+
+  decodeLink: function() {
+    var link = this.attrs.linkUrl.value || this.attrs.linkUrl;
+    return link.replace(/&amp;/g, "&");
+  }
+});
