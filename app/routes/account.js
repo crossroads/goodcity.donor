80e9da90164c613df07579442113e82f9a9897ca
Patrick <patrixr@users.noreply.github.com>
2022-10-01 16:19:39 +0800

    ADD: my account page

---
+++ app/routes/account.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import AuthorizeRoute from './authorize'
+
+export default AuthorizeRoute.extend({
+  session: Ember.inject.service(),
+
+  setupController(controller) {
+    controller.set('isEditing', false);
+
+    const user = this.get('session').get("loggedInUser");
+    const mobile = user.get("mobile");
+
+    if (!/^\+852/.test(mobile)) {
+      //  @tofix hack, we should really be keeping the country code in order to save the records
+      user.set("mobile", "+852" + mobile)
+    }
+  },
+
+  renderTemplate() {
+    this.render(); // default template
+
+    if(this.get('session.isLoggedIn')) {
+      this.render('appMenuList', {
+        into: 'account',
+        outlet: 'appMenuList',
+        controller: 'application'
+      });
+    }
+  }
+});
